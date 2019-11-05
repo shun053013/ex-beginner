d32eadf97879671ea31cdc7d621893fd0da3594d
shun053013 <shun053013@gmail.com>
2019-11-05 14:15:38 +0900

    exam02

--- src/main/java/com/example/Controller/exam02.java
+++ src/main/java/com/example/Controller/exam02.java
@@ -1,5 +1,32 @@
 package com.example.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex-02")
 public class exam02 {
+
+	@RequestMapping("")
+	public String index() {
+		return "exam02";
+	}
+
+	@RequestMapping("input")
+	public String Input(Integer num1, Integer num2, Model model) {
+		model.addAttribute("num1", num1);
+		model.addAttribute("num2", num2);
+
+		int answer = num1 + num2;
+		model.addAttribute("answer", answer);
+
+		return "exam02-result";
+
+	}
+	@RequestMapping("/to-page2")
+	public String toPage() {
+		return "exam02-result2";
+	}
 
 }
